f46026c6a0732b44d7f7cf2a1842d196daf4197c
Mohamed Belal <hero.armna@gmail.com>
2024-05-05 13:11:25 +0200

    init commit

---
+++ database/migrations/2024_05_05_104552_create_course_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->id();
+            $table->string('titile');
+            $table->text('description');
+            $table->unsignedSmallInteger('duration');
+            $table->boolean('is_approved')->default(false);
+            $table->unsignedSmallInteger('price');
+            $table->enum('lang', ['arabic', 'english']);
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('teacher_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course');
+    }
+};
---
+++ database/migrations/2024_05_05_105825_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('course');
+            $table->text('comments')->nullable();
+            $table->unsignedTinyInteger('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
